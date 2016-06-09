9384753a54352d08086ac70b985ee34ca17e2848
andresjose1983 <andresjose1983@gmail.com>
2016-06-09 16:05:43 -0400

    Added animation on item product view

--- app/src/main/java/com/pskloud/osm/util/Functions.java
+++ app/src/main/java/com/pskloud/osm/util/Functions.java
@@ -1,5 +1,6 @@
 package com.pskloud.osm.util;
 
+import android.animation.ObjectAnimator;
 import android.graphics.Bitmap;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
@@ -7,6 +8,7 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.animation.ViewPropertyAnimation;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.pskloud.osm.OsmApplication;
 import com.pskloud.osm.R;
@@ -25,6 +27,7 @@ public final class Functions {
                 .centerCrop()
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .placeholder(R.mipmap.ic_file_download_black_48dp)
+                .animate(animationObject)
                 .into(new BitmapImageViewTarget(imageView) {
                     @Override
                     protected void setResource(Bitmap resource) {
@@ -36,5 +39,16 @@ public final class Functions {
                     }
                 });
     }
+
+    static ViewPropertyAnimation.Animator animationObject = view -> {
+            // if it's a custom view class, cast it here
+            // then find subviews and do the animations
+            // here, we just use the entire view for the fade animation
+            view.setAlpha( 0f );
+
+            ObjectAnimator fadeAnim = ObjectAnimator.ofFloat( view, "alpha", 0f, 1f );
+            fadeAnim.setDuration( 2500 );
+            fadeAnim.start();
+    };
 
 }

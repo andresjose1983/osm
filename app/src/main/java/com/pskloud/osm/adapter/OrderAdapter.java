c57bd2f03e00e6daa0fd742b409fbee801a37cf4
andresjose1983 <andresjose1983@gmail.com>
2016-06-10 08:55:32 -0400

    Added product items on order view

--- app/src/main/java/com/pskloud/osm/adapter/OrderAdapter.java
+++ app/src/main/java/com/pskloud/osm/adapter/OrderAdapter.java
@@ -1,10 +1,13 @@
 package com.pskloud.osm.adapter;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Filterable;
 import android.widget.TextView;
 
 import com.pskloud.osm.R;
@@ -27,14 +30,108 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.OrderHolder>
     public class OrderHolder extends RecyclerView.ViewHolder {
 
         private TextView mTvNumber;
-        private TextView mTvQuantity;
         private TextView mTvDate;
+        private RecyclerView mRvProducts;
+        private RecyclerView.LayoutManager mLayoutManager;
+        private ProductAdapter mProductAdapter;
+        private View mvProducts;
+
 
         public OrderHolder(View itemView) {
             super(itemView);
             mTvNumber = (TextView) itemView.findViewById(R.id.tv_number);
-            mTvQuantity = (TextView) itemView.findViewById(R.id.tv_quantity);
             mTvDate = (TextView) itemView.findViewById(R.id.tv_date);
+            mRvProducts = (RecyclerView) itemView.findViewById(R.id.rv_products);
+            mvProducts = itemView.findViewById(R.id.v_products);
+
+            itemView.setOnClickListener(v -> {
+                Order order = mOrders.get(getAdapterPosition());
+                if (order.isView()) {
+                    order.setView(false);
+                    //collapse();
+                } else {
+                    order.setView(true);
+                    //expand();
+                }
+                mOrders.set(getAdapterPosition(), order);
+                notifyItemChanged(getAdapterPosition());
+            });
+
+            mLayoutManager = new LinearLayoutManager(mRvProducts.getContext());
+            mRvProducts.setLayoutManager(mLayoutManager);
+            mRvProducts.setItemAnimator(new DefaultItemAnimator());
+            mRvProducts.setHasFixedSize(true);
+        }
+
+        private void expand() {
+            //set Visible
+            mvProducts.setVisibility(View.VISIBLE);
+
+            final int widthSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+            final int heightSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+            mvProducts.measure(widthSpec, heightSpec);
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+                ValueAnimator mAnimator = slideAnimator(0, mvProducts.getMeasuredHeight());
+                mAnimator.start();
+            }
+        }
+
+        private ValueAnimator slideAnimator(int start, int end) {
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+                ValueAnimator animator = ValueAnimator.ofInt(start, end);
+
+                animator.addUpdateListener(valueAnimator -> {
+                    //Update Height
+                    int value = 0;
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+                        value = (Integer) valueAnimator.getAnimatedValue();
+                    }
+                    ViewGroup.LayoutParams layoutParams = mvProducts.getLayoutParams();
+                    layoutParams.height = value;
+                    mvProducts.setLayoutParams(layoutParams);
+                });
+                return animator;
+            }
+            return null;
+        }
+
+        private void collapse() {
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+
+                ValueAnimator mAnimator = slideAnimator(mvProducts.getHeight(), 0);
+                if (mAnimator != null) {
+                    mAnimator.addListener(new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animator) {
+                            //Height=0, but it set visibility to GONE
+                            mvProducts.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {
+                        }
+                    });
+
+                }
+
+                mAnimator.start();
+            } else {
+                mvProducts.setVisibility(View.GONE);
+            }
+
         }
     }
 
@@ -52,8 +149,15 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.OrderHolder>
         Order order = mOrders.get(position);
 
         holder.mTvNumber.setText(order.getNumber());
-        holder.mTvQuantity.setText(String.valueOf(order.getTotalItem()));
         holder.mTvDate.setText(Functions.format(order.getDate()));
+
+        holder.mProductAdapter = new ProductAdapter(order.getProducts());
+        holder.mRvProducts.setAdapter(holder.mProductAdapter);
+
+        if(order.isView())
+            holder.expand();
+        else
+            holder.collapse();
 
     }
 
--- app/src/main/java/com/pskloud/osm/model/Order.java
+++ app/src/main/java/com/pskloud/osm/model/Order.java
@@ -3,7 +3,10 @@ package com.pskloud.osm.model;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.pskloud.osm.util.Functions;
+
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by andres on 09/06/16.
@@ -15,6 +18,10 @@ public class Order implements Parcelable{
     private Date date;
 
     private int totalItem;
+
+    private boolean isView;
+
+    private List<Product> products = Functions.getProduct();
 
     public Order(String number, Date date, int totalItem) {
         this.number = number;
@@ -46,6 +53,23 @@ public class Order implements Parcelable{
         this.totalItem = totalItem;
     }
 
+    public boolean isView() {
+        return isView;
+    }
+
+    public void setView(boolean view) {
+        isView = view;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+
     @Override
     public int describeContents() {
         return 0;
@@ -56,6 +80,8 @@ public class Order implements Parcelable{
         dest.writeString(this.number);
         dest.writeLong(this.date != null ? this.date.getTime() : -1);
         dest.writeInt(this.totalItem);
+        dest.writeByte(this.isView ? (byte) 1 : (byte) 0);
+        dest.writeTypedList(this.products);
     }
 
     protected Order(Parcel in) {
@@ -63,6 +89,8 @@ public class Order implements Parcelable{
         long tmpDate = in.readLong();
         this.date = tmpDate == -1 ? null : new Date(tmpDate);
         this.totalItem = in.readInt();
+        this.isView = in.readByte() != 0;
+        this.products = in.createTypedArrayList(Product.CREATOR);
     }
 
     public static final Creator<Order> CREATOR = new Creator<Order>() {
